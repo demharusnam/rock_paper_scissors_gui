fcda8721499a286384bb438613d4172552f7d224
Mansur Ahmed <ahmedm.dev@gmail.com>
2019-11-13 19:34:15 -0500

    rework result computation, and make UI scoreboard dynamic

--- script.js
+++ script.js
@@ -1,75 +1,93 @@
+// retrieves game buttons to know which is pressed
+const buttons = Array.from(document.querySelectorAll('.btn'));
+// retrieves scoreboard information, [0] = Player:, [1] = Tie:, [2] = Computer:
+const paragraphs = Array.from(document.querySelectorAll('.scoreboard'));
+// retrieves tag holding text for round result
+const roundStatus = document.querySelector('#roundresult');
+// retrieves tag holding text for game status/greeting
+const gameStatus = document.querySelector('#gameStatus');
+
 // stores all available choices for game
-const choice = ["rock", "paper", "scissors"];
+const choice = ["Rock", "Paper", "Scissors"];
 // tracks game scores
 let winCount = 0;
 let lossCount = 0;
 let roundCount = 0;
-// retrieves game buttons to know which is pressed
-const buttons = Array.from(document.querySelectorAll('.btn'));
+
 // randomly decides which of the available options the computer picks
 let computerPlay = () => choice[Math.floor(Math.random(2)*3)];
-// compares player choice with computer choice and returns result
+
+// compares player choice with computer choice and updates result to UI
 // represents one round of the game
 let playRound = (playerSelection, computerSelection) => {
 	roundCount++;
-	switch(playerSelection) {
-		case "rock":
-			if (computerSelection == "rock") {
-				 console.log("Round tie. Rock and Rock");
-			} else if (computerSelection == "paper") {
-				console.log("Round lost. Paper beats Rock");
-				lossCount++;
-			} else if (computerSelection == "scissors") {
-				console.log("Round won! Rock beats Scissors");
-				winCount++;
-			}
+
+	switch (choiceComparison(playerSelection, computerSelection)) {
+		case 0:
+			roundStatus.textContent = `Round lost. ${playerSelection} and ${computerSelection}`;
+			lossCount++;
+			paragraphs[2].textContent = `Computer: ${lossCount}`;
 			break;
-		case "paper":
-			if (computerSelection == "paper") {
-				console.log("Round tie. Paper and Paper");
-			}
-			else if (computerSelection == "scissors") {
-				console.log("Round lost. Scissors beats Paper");
-				lossCount++;
-			}
-			else if (computerSelection == "rock") {
-				console.log("Round won! Paper beats Rock");
-				winCount++;
-			}
+		case 1:
+			roundStatus.textContent = `Round tie. ${playerSelection} and ${computerSelection}`;
+			paragraphs[1].textContent = `Tie: ${roundCount - winCount - lossCount}`;
 			break;
-		case "scissors":
-			if (computerSelection == "scissors") {
-				console.log("Round tie. Scissors and Scissors");
-			}
-			else if (computerSelection == "rock") {
-				console.log("Round lost. Rock beats Scissors");
-				lossCount++;
-			}
-			else if (computerSelection == "paper") {
-				console.log("Round won! Scissors beats Paper");
-				winCount++;
-			}
-			break;
-		default:
+		case 2:
+			roundStatus.textContent = `Round won! ${playerSelection} and ${computerSelection}`;
+			winCount++;
+			paragraphs[0].textContent = `Player: ${winCount}`;
 			break;
 	}
 	// declares game winner after either best of 5 matches have been won
 	// or a tie has been reached. Then, the game resets.
 	if (winCount > 2) {
-		console.log("Game over. You won!");
-		winCount = 0;
-		lossCount = 0;
-		roundCount = 0;
+		gameStatus.textContent = `Game Over! You won ${winCount}-${lossCount}!`;
+		resetGame();
 	} else if (lossCount > 2) {
-		console.log("Game over. You lost.");
-		winCount = 0;
-		lossCount = 0;
-		roundCount = 0;
-	} else if (winCount == lossCount && roundCount > 4) {
-		console.log("Game over. It's a tie.");
-		winCount = 0;
-		lossCount = 0;
-		roundCount = 0;
+		gameStatus.textContent = `Game Over! You lost ${winCount}-${lossCount}.`;
+		resetGame();
+	} else if (roundCount > 4) {
+		if (winCount == lossCount) {
+			gameStatus.textContent = `Game Over! You tied ${winCount}-${lossCount}.`;	
+		} else if (winCount < lossCount) {
+			gameStatus.textContent = `Game Over! You lost ${winCount}-${lossCount}.`;	
+		} else {
+			gameStatus.textContent = `Game Over! You won ${winCount}-${lossCount}!`;	
+		}
+		resetGame();
+	}
+}
+
+resetGame = () => {
+	winCount = 0;
+	lossCount = 0;
+	roundCount = 0;
+	paragraphs[1].textContent = `Tie:`;
+	paragraphs[2].textContent = `Computer:`;
+	paragraphs[0].textContent = `Player:`;
+	roundStatus.textContent = "Play again!";
+}
+
+// compares player and comuter choices and returns a result
+// 0 = player loss, 1 = tie, 2 = player win
+choiceComparison = (player, computer) => {
+	const pVal = player.length;
+	const cVal = computer.length;
+
+	if (pVal == cVal) { //same value
+		return 1;
+	} else if (pVal*2 == cVal) { //rock*2 == scissors
+		return 2;
+	} else if (pVal*2 > cVal && cVal == 5) { //rock*2 > paper && paper
+		return 0;
+	} else if (pVal > cVal && cVal == 4) { //paper > rock && rock
+		return 2;
+	} else if (pVal < cVal && cVal == 8) { //paper < scissors && scissors
+		return 0;
+	} else if (pVal == cVal*2) { //scissors == rock*2
+		return 0;
+	} else if (pVal > cVal && cVal == 5) { //scissors > paper && paper
+		return 2;
 	}
 }
 
